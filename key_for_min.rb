a8befce23bdac80018d054173dbca4e9c1f716b6
Elaina <elainaseki@gmail.com>
2019-02-18 23:40:15 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,7 @@ def key_for_min_value(name_hash)
  min_v = nil
   min_k = nil
   name_hash.each do |k, v|
-    if min_v == nil
-      min_v = v
-      min_k = k
-    elsif min_v > v
+    if min_v == nil || min_v > v
       min_v = v
       min_k = k
     end
